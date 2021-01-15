717ea75aeead336cfb992a53c6518bb5baaf73b1
Cristian Espinoza L <cpespinozalo@utpl.edu.ec>
2021-01-14 20:27:31 -0500

    Se agrega proyecto base para trabajo grupal

---
+++ AdopcionMascotas/src/adopcionmascotas/AdopcionMascotas.java
@@ -0,0 +1,18 @@
+package adopcionmascotas;
+
+/**
+ *
+ * @author Grupo 3 - Ingenieria de Software
+ * @universidad Universidad Tecnica Particular de Loja
+ * @version 1.0
+ */
+public class AdopcionMascotas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
