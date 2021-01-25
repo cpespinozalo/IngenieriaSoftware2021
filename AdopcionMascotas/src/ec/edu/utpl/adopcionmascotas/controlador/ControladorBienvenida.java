1f92e9431d6ecdaae70f2030f6d5a5a95b267573
Cristian Espinoza L <cpespinozalo@utpl.edu.ec>
2021-01-25 00:15:55 -0500

    Se agrega interfaces de pantallas de Bienvenida y Recuperacion de Contraseña

---
+++ AdopcionMascotas/src/ec/edu/utpl/adopcionmascotas/controlador/ControladorBienvenida.java
@@ -0,0 +1,88 @@
+package ec.edu.utpl.adopcionmascotas.controlador;
+
+import ec.edu.utpl.adopcionmascotas.modelo.bd.Cliente;
+import ec.edu.utpl.adopcionmascotas.modelo.pojo.Sesion;
+import java.io.Serializable;
+import java.util.List;
+import javax.swing.JComboBox;
+
+
+/**
+ * Clase Controlador de Bienvenida
+ * 
+ * @author Grupo 3 - Ingeniería de Software
+ * @version 1.0
+ */
+public class ControladorBienvenida implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private Utilitarios util;
+    private Sesion sesion;
+    private Integer cusuario;
+    private String usuario;
+
+    
+    private static final String SQL_SELECT_SESION = "SELECT USU.NOMBRES || ' ' || USU.APELLIDOS NOMBRELEGAL, USU.GENERO "
+            + "FROM MASCOTAS.TUSUARIO USU "
+            + "WHERE USU.USUARIO=? ";
+    
+    private static final String SQL_SELECT_ROLES = "SELECT ROL.CROL, ROL.NOMBRE, ROL.DESCRIPCION "
+            + "FROM MASCOTAS.TROLUSUARIO TRU, MASCOTAS.TROL ROL "
+            + "WHERE TRU.CUSUARIO=? "
+            + "AND TRU.CROL=ROL.CROL "
+            + "AND TRU.ACTIVO=1 ";
+
+    public ControladorBienvenida(Sesion sesion, Integer cusuario, String usuario) {
+
+        this.sesion = sesion;
+        this.util = new Utilitarios();
+        this.cusuario = cusuario;
+        this.usuario = usuario;
+        setSesion();
+    }
+
+    private void setSesion(){
+        
+        List<Object> datos =  getDatosSesion(usuario);
+        for(Object dato : datos){
+            Object [] arreglo;
+            arreglo = (Object [])dato;
+            
+            sesion.setNombreUsuario(arreglo[0].toString());
+            sesion.setGenero(arreglo[1].toString());
+        } 
+        sesion.setSistema("Sistema Operativo: " + System.getProperty("os.name") + " - Arquitectura: " + System.getProperty("os.arch"));
+        sesion.setFinicio(util.tiempoSistema("dd-MM-yyyy HH:mm:ss"));
+        sesion.setResultado("LOGIN EXITOSO");
+        sesion.newSesion();
+    }
+    
+    public void getDatosCombo(JComboBox<String> combo) {
+       
+        List<Object> datos =  getRolUsuario(cusuario);
+        for(Object dato : datos){
+            Object [] arreglo;
+            arreglo = (Object [])dato;
+            sesion.setCrol(Integer.parseInt(arreglo[0].toString()));
+            sesion.setNombreRol(arreglo[1].toString());
+            sesion.setDescripcionRol(arreglo[2].toString());
+            combo.addItem(arreglo[2].toString());
+        }
+    }
+    
+    public List<Object> getDatosSesion(String usuario){
+        
+        List<Object> resultado;
+        Cliente cliente = new Cliente();
+        resultado = cliente.query(SQL_SELECT_SESION, usuario);
+        return resultado;
+    }
+    
+    public List<Object> getRolUsuario(Integer cusuario){
+        
+        List<Object> resultado;
+        Cliente cliente = new Cliente();
+        resultado = cliente.query(SQL_SELECT_ROLES, cusuario);
+        return resultado;
+    }   
+}
---
+++ AdopcionMascotas/src/ec/edu/utpl/adopcionmascotas/controlador/ControladorRecuperacion.java
@@ -0,0 +1,111 @@
+package ec.edu.utpl.adopcionmascotas.controlador;
+
+import ec.edu.utpl.adopcionmascotas.modelo.bd.Cliente;
+import java.util.List;
+
+/**
+ * Clase Controlador de Recuperacion de Contraseña de la Aplicacion
+ * 
+ * @author Grupo 3 - Ingeniería de Software
+ * @version 1.0
+ */
+public class ControladorRecuperacion {
+    
+   
+    private List<Object> datos;
+    public Integer cusuario;
+    public String usuario;
+    public String identificacion;
+    public String estado;
+    public Integer pregunta;
+    public String respuesta;
+    
+    private static final String SQL_SELECT_USUARIO = "SELECT USU.IDENTIFICACION, USU.ESTADO, TPU.RESPUESTA, USU.CUSUARIO "
+            + "FROM MASCOTAS.TUSUARIO USU, MASCOTAS.TPREGUNTASUSUARIO TPU "
+            + "WHERE USU.CUSUARIO=TPU.CUSUARIO "
+            + "AND USU.USUARIO=? "
+            + "AND TPU.CPREGUNTA=? "
+            + "AND USU.ACTIVO=1 "
+            + "AND TPU.ACTIVO=1 ";
+    
+    private static final String SQL_SELECT_PREGUNTA = "SELECT PREGUNTA "
+            + "FROM MASCOTAS.TPREGUNTA "
+            + "WHERE CPREGUNTA=? "
+            + "AND ACTIVO=1 ";
+    
+    private static final String UPDATE_TUSUARIO = "UPDATE MASCOTAS.TUSUARIO "
+            + "SET PASSWORD=? "
+            + "WHERE CUSUARIO=? "
+            + "AND ACTIVO=1 ";
+    
+    public ControladorRecuperacion(Integer pregunta) {
+
+        this.pregunta = pregunta;
+    }
+    
+    private List<Object> getDatosUsuario(String usuario){
+        
+        List<Object> resultado;
+        Cliente cliente = new Cliente();
+        resultado = cliente.query(SQL_SELECT_USUARIO, usuario, pregunta);
+        return resultado;
+    }
+    
+    public boolean existeUsuario(String usuario) throws Exception{
+        
+        boolean existe = false;
+        this.datos =  getDatosUsuario(usuario);
+        if(!datos.isEmpty()){
+            setUsuarioData(datos);
+            existe = true;
+        }
+        return existe;
+    }
+    
+    public boolean savePassword(String password) throws Exception{
+        
+        CifradoAes aes = new CifradoAes();
+        String passwordCifrada = aes.encriptar(password);
+        Cliente cliente = new Cliente();
+        return cliente.execute(UPDATE_TUSUARIO, passwordCifrada, this.cusuario) > 0;
+    }
+    
+    public boolean validarIdentificacion(String id) throws Exception{
+        
+        return identificacion.equals(id);
+    }
+
+    public boolean validarRespuesta(String resp) throws Exception{
+        
+        CifradoAes aes = new CifradoAes();
+        String respuestacifrada = aes.encriptar(resp);
+        return respuestacifrada.equals(this.respuesta);
+    }
+    
+    private void setUsuarioData(List<Object> dataList) throws Exception{
+        
+        for(Object dato : dataList){
+            Object [] arreglo;
+            arreglo = (Object [])dato;
+            this.identificacion = arreglo[0].toString();
+            this.estado = arreglo[1].toString();
+            this.respuesta = arreglo[2].toString();
+            this.cusuario = Integer.parseInt(arreglo[3].toString());
+        }
+    }
+    
+    public String getPregunta(){
+        
+        String preguntaDes = "";
+        List<Object> resultado;
+        Cliente cliente = new Cliente();
+        resultado = cliente.query(SQL_SELECT_PREGUNTA, this.pregunta);
+        if(!resultado.isEmpty()){
+            for(Object dato : resultado){
+                preguntaDes = dato.toString();
+            }
+        }
+        return preguntaDes;
+    }
+
+}
