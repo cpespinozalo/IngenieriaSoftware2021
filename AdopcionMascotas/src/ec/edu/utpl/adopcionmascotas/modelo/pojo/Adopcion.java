ef2c97192cf59f4b8f48d33042fbd8de7e0e110f
CristinaContreras <77370888+CristinaContreras@users.noreply.github.com>
2021-01-28 17:42:58 -0500

    Se crea la clase adopcion

---
+++ AdopcionMascotas/src/ec/edu/utpl/adopcionmascotas/modelo/pojo/Adopcion.java
@@ -0,0 +1,78 @@
+package ec.edu.utpl.adopcionmascotas.modelo.pojo;
+
+import ec.edu.utpl.adopcionmascotas.modelo.bd.Cliente;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Adopcion implements Serializable{
+    
+    private Auditoria auditoria;
+    
+    private static final long serialVersionUID = 1L;
+    private Integer sesionId;
+    private Integer cadopcion;
+    private Integer cmascota;
+    private String cusuarioadopcion;
+    private String fadopcion;
+    private String estadoadopcion;
+
+    public Auditoria getAuditoria() {
+        return auditoria;
+    }
+
+    public void setAuditoria(Auditoria auditoria) {
+        this.auditoria = auditoria;
+    }
+
+    public Integer getSesionId() {
+        return sesionId;
+    }
+
+    public void setSesionId(Integer sesionId) {
+        this.sesionId = sesionId;
+    }
+
+    public Integer getCadopcion() {
+        return cadopcion;
+    }
+
+    public void setCadopcion(Integer cadopcion) {
+        this.cadopcion = cadopcion;
+    }
+
+    public Integer getCmascota() {
+        return cmascota;
+    }
+
+    public void setCmascota(Integer cmascota) {
+        this.cmascota = cmascota;
+    }
+
+    public String getCusuarioadopcion() {
+        return cusuarioadopcion;
+    }
+
+    public void setCusuarioadopcion(String cusuarioadopcion) {
+        this.cusuarioadopcion = cusuarioadopcion;
+    }
+
+    public String getFadopcion() {
+        return fadopcion;
+    }
+
+    public void setFadopcion(String fadopcion) {
+        this.fadopcion = fadopcion;
+    }
+
+    public String getEstadoadopcion() {
+        return estadoadopcion;
+    }
+
+    public void setEstadoadopcion(String estadoadopcion) {
+        this.estadoadopcion = estadoadopcion;
+    }
+    
+    
+    
+}
